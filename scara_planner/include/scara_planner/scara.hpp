b6bf2f823a31c349d5c31b9a1e2c50ca277c2a5a
mpheng666 <mpheng96@gmail.com>
2021-12-27 18:52:55 +0800

    add custom msg

--- scara_planner/include/scara_planner/scara.hpp
+++ scara_planner/include/scara_planner/scara.hpp
@@ -11,6 +11,12 @@
 #include <time.h>
 #include <ros/ros.h>
 
+#include <moveit/move_group_interface/move_group_interface.h>
+#include <moveit/planning_scene_interface/planning_scene_interface.h>
+#include <moveit_msgs/DisplayRobotState.h>
+#include <moveit_msgs/DisplayTrajectory.h>
+#include <moveit_visual_tools/moveit_visual_tools.h>
+
 #include <boost/thread.hpp>
 
 #include "std_msgs/Float32.h"
